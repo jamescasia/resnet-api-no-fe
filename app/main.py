fafe552359287381d6ea69f64c5d52a0583b93e2
jamescasia <jamescasia99@gmail.com>
2023-01-18 23:18:31 +0800

    Update structure

--- app/main.py
+++ app/main.py
@@ -1,49 +1,55 @@
-import torch
 from torchvision.models import resnet50, ResNet50_Weights
 from flask import Flask, request ,render_template, url_for
-import numpy as np
-from werkzeug.utils import secure_filename
-from PIL import Image 
+from werkzeug.utils import secure_filename 
 import urllib.request,io 
+from PIL import Image 
 import os 
 
 def create_app():
 
-    app = Flask('image-classifier')
-
-    UPLOAD_FOLDER = './'
-    ALLOWED_EXTENSIONS =  {'png', 'jpg', 'jpeg'}
+    app = Flask('image-classifier') 
 
     weights = ResNet50_Weights.DEFAULT
     model = resnet50(weights=weights)
     model.eval()
     preprocess = weights.transforms()
-    
-    
-    @app.route('/')  
-    @app.route('/classification/gui', methods=['GET'])
-    def gui():
-        return render_template('classification/gui.html')
 
-    @app.route('/classification/cli', methods=['GET'])
+    @app.route('/classify', methods=['GET'])
+    def classify_link(): 
+        url = io.BytesIO(urllib.request.urlopen(request.get_json()['url']).read())
+        img = Image.open(url)
+        
+        batch = preprocess(img).unsqueeze(0) 
+        prediction = model(batch).squeeze(0).softmax(0)
+        class_id = prediction.argmax().item()
+        score = prediction[class_id].item()
+        category_name = weights.meta["categories"][class_id] 
+        return  { 'class': category_name, 'confidence': float("{:.2f}".format(score)) } 
+  
+    @app.route('/')  
+    @app.route('/gui', methods=['GET'])
+    def gui():
+        return render_template('gui.html')
+
+    @app.route('/cli', methods=['GET'])
     def cli():
-        return render_template('classification/cli.html')
+        return render_template('cli.html')
     
     @app.route('/about', methods=['GET'])
     def about():
         return render_template('about.html'  ) 
     
     
-    @app.route('/classification/gui', methods=['POST']) 
+    @app.route('/gui', methods=['POST']) 
     @app.route('/', methods=['POST']) 
     def classify():
         clear_dir('./static/images/resnet/') 
         if 'file' not in request.files:
-            return render_template('classification/gui.html', msg='No file found')
+            return render_template('gui.html', msg='No file found')
 
         file = request.files['file']
         if file.filename == '':
-            return render_template('classification/gui.html', msg='No file selected')
+            return render_template('gui.html', msg='No file selected')
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -59,30 +65,18 @@ def create_app():
             category_name = weights.meta["categories"][class_id]  
 
             return render_template(
-                'classification/gui.html', result = f"{category_name}: {100 * score:.1f}%", 
+                'gui.html', result = f"{category_name}: {100 * score:.1f}%", 
                 img_url = f"{url_for('static', filename=f'/images/resnet/{filename}')}"
             ) 
 
     
 
-    @app.route('/classify', methods=['POST', 'GET'])
-    def classify_link(): 
-        url = io.BytesIO(urllib.request.urlopen(request.get_json()['url']).read())
-        img = Image.open(url)
-        
-        batch = preprocess(img).unsqueeze(0) 
-        prediction = model(batch).squeeze(0).softmax(0)
-        class_id = prediction.argmax().item()
-        score = prediction[class_id].item()
-        category_name = weights.meta["categories"][class_id] 
-        return  f"{category_name}: {100 * score:.1f}%" 
 
-    
     def clear_dir(dir_path):
         for file in os.listdir(dir_path):
             os.remove(f'{dir_path}/{file}') 
     
     def allowed_file(filename):
-        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
     
     return app
