42ca768781a9c8df754c2f034ab3fb7b8174c2ed
jamescasia <jamescasia99@gmail.com>
2023-01-19 22:22:41 +0800

    Add hostname

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 source env/bin/activate
 export FLASK_APP="app.main:create_app"
 export FLASK_RUN_PORT=8080
+export FLASK_APP_HOSTNAME="0.0.0.0"
 echo $VIRTUAL_ENV
-flask run
+flask run --host=$FLASK_APP_HOSTNAME
