50667816c660d277592f9f01daabdc47101f06ac
jamescasia <jamescasia99@gmail.com>
2023-01-23 15:33:57 +0800

    Refactor for tutorial article

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from torchvision.models import resnet50, ResNet50_Weights
-from flask import Flask, request  
+from flask import Flask, request ,render_template, url_for
+from werkzeug.utils import secure_filename 
 import urllib.request,io 
-from PIL import Image  
+from PIL import Image 
+import os 
 
 def create_app():
 
@@ -12,7 +14,7 @@ def create_app():
     model.eval()
     preprocess = weights.transforms()
 
-    @app.route('/classify', methods=['GET'])
+    @app.route('/classify')
     def classify(): 
         url = io.BytesIO(urllib.request.urlopen(request.get_json()['url']).read())
         img = Image.open(url)
@@ -23,5 +25,46 @@ def create_app():
         score = prediction[class_id].item()
         category_name = weights.meta["categories"][class_id] 
         return  { 'class': category_name, 'confidence': float("{:.2f}".format(score)) } 
- 
+    
+    @app.route('/')   
+    def gui():
+        return render_template('gui.html')
+
+    @app.route('/', methods=['POST'])  
+    def classify_gui():
+        clear_dir('./static/images/') 
+        if 'file' not in request.files:
+            return render_template('gui.html', msg='No file found')
+
+        file = request.files['file']
+        if file.filename == '':
+            return render_template('gui.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = f'static/images/{filename}'
+            file.save(path)
+
+            img = Image.open(path)    
+            batch = preprocess(img).unsqueeze(0) 
+    
+            prediction = model(batch).squeeze(0).softmax(0)
+            class_id = prediction.argmax().item()
+            score = prediction[class_id].item()
+            category_name = weights.meta["categories"][class_id]  
+
+            return render_template(
+                'gui.html', result = f"{category_name}: {100 * score:.1f}%", 
+                img_url = f"{url_for('static', filename=f'/images/{filename}')}"
+            ) 
+
     return app
+
+
+def clear_dir(dir_path):
+    for file in os.listdir(dir_path):
+        os.remove(f'{dir_path}/{file}') 
+    
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+    
